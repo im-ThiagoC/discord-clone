22de05605189e98ee3dd7520fa7e886068a50e76
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-03-15 16:13:11 -0300

    login and register page has been created

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p className="text-3xl font-bold text-indigo-500">
+      This is a Discord Clone in Next.js
+      </p>
+      <Button className="">
+        Click me!
+      </Button>
+    </div>
+    
+  )
+}

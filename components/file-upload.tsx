2968cf3be66c17d26f2ab9b5334916b9d96222b1
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-03-25 16:30:04 -0300

    add: upload server image

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { Endpoint } from "next/dist/build/swc"
-import { UploadDropzone } from "@/lib/uploadthing";
+import { UploadDropzone } from "@/lib/uploadthing"
 import "@uploadthing/react/styles.css"
+import { X } from "lucide-react"
+import Image from "next/image"
 
 interface FileUploadProps {
     onChange: (url?: string) => void;
@@ -16,7 +18,31 @@ export const FileUpload = ({
     endpoint,
 }: FileUploadProps
 ) => {
+    const fileType = value?.split(".").pop();
+
+    if(value && fileType !== "pdf"){
+        return (
+            <div className="relative h-20 w-20">
+                <Image
+                    src={value}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="Upload"
+                    className="rounded-full"
+                />
+                <button onClick={() => onChange()}
+                    className="bg-rose-500 text-white rounded-full p-1 absolute top-0 right-0 shadow-sm"
+                    type="button"
+                >
+                    <X
+                        className="h-4 w-4"
+                    />
+                </button>
+            </div>
+        )
+    }
     return (
+
         <UploadDropzone
             endpoint={endpoint}
             onClientUploadComplete={(res) => {
--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -9,7 +9,7 @@ export const initialProfile = async () => {
     return redirectToSignIn();
   }
 
-  const profile = await db.profile.findUnique({
+  const profile = await db.profile.findUniqueOrThrow({
     where: {
       userId: user.id,
     }

5df03ea24c0a76c5505cc7b5e805dc87ac02cb71
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-03-24 23:35:50 -0300

    feature: light/dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
@@ -7,6 +8,7 @@ export default function Home() {
       <UserButton 
         afterSignOutUrl="/"
         />
+      <ModeToggle />
       <p className="text-3xl font-bold text-indigo-500">
       Discord
       </p>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider  } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -17,8 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(
+          font.className,
+          "bg-white dark:bg-[#313338] text-black dark:text-white transition-colors duration-200  "
+          )}>
+          <ThemeProvider 
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            storageKey="discord-theme"
+            >
+            {children}  
+          </ThemeProvider>
+          
+        </body>
      </html>
     </ClerkProvider>
     

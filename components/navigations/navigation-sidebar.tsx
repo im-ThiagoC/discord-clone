4222bf5e8d6f1c5f7543ff07ae7b9f21bebbcaa6
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-03-29 22:49:33 -0300

    Sidebar Finished

--- components/navigations/navigation-sidebar.tsx
+++ components/navigations/navigation-sidebar.tsx
@@ -4,6 +4,10 @@ import { redirect } from "next/navigation";
 
 import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
+import { ModeToggle } from "../mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export const NavigationSideBar = async () => {
     const profile = await currentProfile();
@@ -23,13 +27,35 @@ export const NavigationSideBar = async () => {
     });
 
     return (
-        <nav className="space-y-4 flex flex-col items-center h-full
+        <div className="space-y-4 flex flex-col items-center h-full
          text-primary w-full dark:bg-[#1E1F22] py-3">
             <NavigationAction />
             <Separator
                 className="h-[2px] bg-zinc-300 dark:bg-zinc-700
                 ronded-md w-10 mx-auto"
             />
-        </nav>
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server) => (
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
+                    </div>
+                ))}
+            </ScrollArea>
+            <div className="pb-3  mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle />
+                <UserButton 
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                            avatarBox: "h-[36px] w-[36px]"
+                            }
+                    }}
+                />
+            </div>
+        </div>
     )
 }

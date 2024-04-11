00fad0a81598bb4979fa05ae82f5651a2e06173d
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-04-11 12:14:12 -0300

    finished server sidebar dropdown menu

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,6 +1,10 @@
 import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 
+import { db } from "@/lib/db";
+import { ChannelType } from "@prisma/client";
+import { ServerHeader } from "@/components/server/server-header";
+
 interface ServerSidebarProps {
     serverId: string;
 }
@@ -14,10 +18,44 @@ export const ServerSidebar = async ({
         return redirect("/");
     }
 
+    const server = await db.server.findUnique({
+        where: {
+            id: serverId,
+        },
+        include: {
+            channels: {
+                orderBy: {
+                    createdAt: "asc",
+                },
+            },
+            members: {
+                include: {
+                    profile: true,
+                },
+                orderBy: {
+                    role: "asc",
+                },
+            },
+        },
+    });
+
+    const textChannels = server?.channels.filter(channel => channel.type === ChannelType.TEXT);
+    const voiceChannels = server?.channels.filter(channel => channel.type === ChannelType.VOICE);
+    const videoChannels = server?.channels.filter(channel => channel.type === ChannelType.VIDEO);
+    const members = server?.members.filter((member) => member.profileId !== profile.id);
+
+    if (!server) {
+        return redirect("/");
+    }
+
+    const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
     return (
-        <div>
-            Server Sidebar Component
+        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+            <ServerHeader
+                server={server}
+                role={role}
+            />
        </div>
     )
 }

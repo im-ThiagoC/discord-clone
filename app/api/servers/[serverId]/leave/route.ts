2ff6d0b54912a30874b55618ec721810b6d0e519
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-04-26 14:10:45 -0300

    new

--- app/api/servers/[serverId]/leave/route.ts
+++ app/api/servers/[serverId]/leave/route.ts
@@ -2,6 +2,14 @@ import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server"
 
+export async function generateStaticParams() {
+    const posts = await fetch('https://.../servers/leave').then((res) => res.json())
+   
+    return posts.map((post:any) => ({
+      slug: post.slug,
+    }))
+  }
+
 export async function PATCH(
     req: Request,
     { params }: { params: { serverId: string }}

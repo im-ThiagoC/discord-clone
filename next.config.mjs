ad98fd52697f9a2bfcb7650dbf1c1430aa75d7ad
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-04-26 13:04:03 -0300

    nextjs config for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
